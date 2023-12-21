e6739fb7075db458e84cd0641e80e47c6ed473dc
joy-c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2023-12-21 16:13:58 -0300

    Timer funcionando e função para audio também

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,6 +4,7 @@ const state = {
 		enemy:document.querySelector(".enemy"),
 		timeLeft: document.querySelector("#time-left"),
 		score: document.querySelector("#score"),
+		lives: document.querySelector("#lives"),
 	},
 
 	values: {
@@ -11,8 +12,37 @@ const state = {
 		gameSpeed: 1000,
 		hitPosition: 0,
 		result: 0,
+		currentTime: 60,
+		lives: 3,
+	},
+
+	actions: {
+		countDownTimerId: setInterval(countDown, 1000),
 	}
 };
+
+function playSound() {
+	let audio = new Audio("./src/audios/hit.m4a");
+	audio.volume = 0.1;
+	audio.play();
+}
+
+function decreasesLives() {
+	state.values.lives--;
+	state.view.lives = state.values.lives;
+}
+
+function countDown() {
+	state.values.currentTime--;
+	state.view.timeLeft.textContent = state.values.currentTime;
+
+	if (state.values.currentTime <= 0) { 
+		clearInterval(state.actions.countDownTimerId);
+		clearInterval(state.values.timerId);
+		decreasesLives();
+		alert('Fim da  partida! Pontuação atual: ' + state.values.result);
+	}
+}
 
 function randomSquare(){
 
@@ -37,6 +67,7 @@ function addListenerHitBox() {
 				state.values.result++;
 				state.view.score.textContent = state.values.result;
 				state.values.hitPosition = null;
+				playSound();
 			}
 		});
 	});
