020e2c496b3ad2a664fff4a0909bd42a2b877cad
joy-c1 <joyce.freitas@estudante.ifgoiano.edu.br>
2023-12-21 10:55:39 -0300

    sistema de pontuação funcionando

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,7 +7,10 @@ const state = {
 	},
 
 	values: {
+		timerId: null,
 		gameSpeed: 1000,
+		hitPosition: 0,
+		result: 0,
 	}
 };
 
@@ -19,20 +22,29 @@ function randomSquare(){
 
 	let randomNumber = Math.floor(Math.random() * 9);
 	let randomSquare = state.view.squares[randomNumber];
-	randomSquare.classList.add("enemy")
+	randomSquare.classList.add("enemy");
+	state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy() {
-	state.values.timer
+	state.values.timerId = setInterval(randomSquare, state.values.gameSpeed);
 }
 
 function addListenerHitBox() {
 	state.view.squares.forEach((square)=> {
+		square.addEventListener("mousedown", () => {
+			if(square.id === state.values.hitPosition){
+				state.values.result++;
+				state.view.score.textContent = state.values.result;
+				state.values.hitPosition = null;
+			}
+		});
 	});
 }
 
 function init() {
-	randomSquare();
+	moveEnemy();
+	addListenerHitBox();
 }
 
 init();
